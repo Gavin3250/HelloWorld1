f1b34042fc0981184caaf189cd581a20b06978ba
Gavin3250 <143124580+Gavin3250@users.noreply.github.com>
2023-09-14 11:41:47 -0700

    Add files via upload

---
+++ Operations/RenameThisClass.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RenameThisClass
+{
+    public static void main(String[] args) {
+        int i = 10;
+        int i += f;
+        
+        
+        
+    }
+}
