437bb8dff2916ea163a1f63fa90d29cbc0e72fc9
Gavin3250 <143124580+Gavin3250@users.noreply.github.com>
2023-09-15 11:40:15 -0700

    Added RenameThisClass.java

---
+++ HelloWorld1/Operations/RenameThisClass.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RenameThisClass
+{
+    public static void main(String[] args) {
+        int i = 10;
+        double f = 2.5;
+        f += i;
+        boolean flag = false;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
+        
+        
+        
+        
+    }
+}
